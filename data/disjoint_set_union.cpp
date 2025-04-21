5b97ac7257be5a9846ab03608635b76922986b23
Adithya V <venkateshadithya007@gmail.com>
2025-04-22 02:17:09 +0530

    update dsu

--- data/disjoint_set_union.cpp
+++ data/disjoint_set_union.cpp
@@ -4,6 +4,11 @@
 // components are identified by their representative, who is the root of the tree
 // we can find the component a node belongs to or combine two components together in O(1) time
 
+// read source for path compression + union by size and union by depth optimizations
+
+// sources:
+// cp-algorithms.com (https://cp-algorithms.com/data_structures/disjoint_set_union.html)
+
 class DSU
 {
     // components are stored as trees
@@ -12,23 +17,23 @@ class DSU
 
 private:
     vector<ll> parent;
-    vector<ll> depth;
     vector<ll> sz;
-    ll comps;
+    ll n_comps;
 
 public:
     DSU(ll n)
     {
         // n nodes
-        // nodes are 1-based indexed
+        // nodes are 1-based indexed by default
 
         parent.resize(n);
-        depth.resize(n);
         sz.resize(n);
-        comps = n - 1;
+
+        // change this to n if you are using 0-based indexed nodes
+
+        n_comps = n - 1;
 
         iota(parent.begin(), parent.end(), 0LL);
-        fill(depth.begin(), depth.end(), 0LL);
         fill(sz.begin(), sz.end(), 1LL);
     };
 
@@ -36,14 +41,14 @@ public:
     // find the representative of the tree which the node belongs to
     // takes O(log(n)) time or almost O(1) time
 
-    ll find_representative(ll node)
+    ll find_rep(ll node)
     {
         if (node == parent[node])
         {
             return node;
         }
 
-        return parent[node] = find_representative(parent[node]);
+        return parent[node] = find_rep(parent[node]);
     }
 
     // union by depth
@@ -52,24 +57,19 @@ public:
 
     void union_trees(ll node1, ll node2)
     {
-        ll rep1 = find_representative(node1);
-        ll rep2 = find_representative(node2);
+        ll rep1 = find_rep(node1);
+        ll rep2 = find_rep(node2);
 
         if (rep1 != rep2)
         {
-            if (depth[rep1] < depth[rep2])
+            if (sz[rep1] < sz[rep2])
             {
                 swap(rep1, rep2);
             }
 
             parent[rep2] = rep1;
             sz[rep1] += sz[rep2];
-            comps--;
-
-            if (depth[rep1] == depth[rep2])
-            {
-                depth[rep1]++;
-            }
+            n_comps--;
         }
     }
 
@@ -77,21 +77,48 @@ public:
 
     bool same_component(ll node1, ll node2)
     {
-        return find_representative(node1) == find_representative(node2);
+        return find_rep(node1) == find_rep(node2);
     }
 
     // get the size of the component
 
-    ll get_size(ll node)
+    ll size(ll node)
     {
-        ll rep = find_representative(node);
+        ll rep = find_rep(node);
         return sz[rep];
     }
 
-    // get the number of components 
+    // get the number of components
 
-    ll get_no_of_comps()
+    ll no_of_comps()
     {
-        return comps;
+        return n_comps;
     }
+
+    // get all the components
+
+    vector<vector<ll>> comps()
+    {
+        ll n = (ll) parent.size();
+        vector<ll> index(n);
+        ll idx = 0;
+
+        for (ll i = 1; i < n; i++)
+        {
+            if (find_rep(i) == i)
+            {
+                index[i] = idx++;
+            }
+        }
+
+        vector<vector<ll>> result(idx);
+
+        for (ll i = 1; i < n; i++)
+        {
+            ll rep = find_rep(i);
+            result[index[rep]].push_back(i);
+        }
+
+        return result;
+    } 
 };
