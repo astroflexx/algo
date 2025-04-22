d97303a153d23686c367bd808eca4be6ae2824da
Adithya V <venkateshadithya007@gmail.com>
2025-04-23 00:16:23 +0530

    new segment tree

--- segtree/segment_tree.cpp
+++ segtree/segment_tree.cpp
@@ -1,283 +1,130 @@
 // Segment Tree
 
-// Used for range queries and updates on an array
+// a data structure for range queries and point updates
 
-// A tree where each node corresponds to a range
-// and we store max/min/sum/product of the range in the node
+// a tree where every node corresponds to a range/segment
+// and we store information about the segment in the node
 
 // building takes O(n) time
 // querying takes O(log(n)) time
+// updating takes O(log(n)) time
+
+// there is a simple sum operation defined as an example
+// to range query sums
+
+// replace it with your definition of Info and op
+
+// define the info to be stored in a node
+// must have a default constructor for Info
+
+struct Info
+{
+    ll val;
+    Info() : val(0) {}
+    Info(ll v) : val(v) {}
+};
+
+// define the operation to be done on 2 nodes
+
+Info op(Info &a, Info &b)
+{
+    return Info(a.val + b.val);
+}
 
 class SegmentTree
 {
 private:
     ll n;
-    vector<ll> tree_min, tree_max, tree_sum, tree_prod;
+    vector<Info> tree;
 
-    void build_min(vector<ll> &arr, ll u, ll l, ll r)
+    void build(ll node, ll l, ll r, vector<ll> &a)
     {
         if (l == r)
         {
-            tree_min[u] = arr[l];
+            tree[node] = Info(a[l]);
+            return;
         }
 
-        else
-        {
-            ll mid = (l + r) / 2;
-            build_min(arr, 2 * u, l, mid);
-            build_min(arr, 2 * u + 1, mid + 1, r);
-            tree_min[u] = min(tree_min[2 * u], tree_min[2 * u + 1]);
-        }
+        ll m = (l + r) / 2;
+        build(2 * node, l, m, a);
+        build(2 * node + 1, m + 1, r, a);
+        tree[node] = op(tree[2 * node], tree[2 * node + 1]);
     }
 
-    void build_max(vector<ll> &arr, ll u, ll l, ll r)
+    void update(ll node, ll l, ll r, ll idx, ll val)
     {
         if (l == r)
         {
-            tree_max[u] = arr[l];
+            tree[node] = Info(val);
+            return;
         }
 
+        ll m = (l + r) / 2;
+
+        if (idx <= m)
+            update(2 * node, l, m, idx, val);
         else
-        {
-            ll mid = (l + r) / 2;
+            update(2 * node + 1, m + 1, r, idx, val);
 
-            build_max(arr, 2 * u, l, mid);
-            build_max(arr, 2 * u + 1, mid + 1, r);
-
-            tree_max[u] = max(tree_max[2 * u], tree_max[2 * u + 1]);
-        }
+        tree[node] = op(tree[2 * node], tree[2 * node + 1]);
     }
 
-    void build_sum(vector<ll> &arr, ll u, ll l, ll r)
+    Info query(ll node, ll l, ll r, ll ql, ll qr)
     {
-        if (l == r)
+        assert(l <= ql && qr <= r);
+
+        if (ql == l && qr == r)
         {
-            tree_sum[u] = arr[l];
+            return tree[node];
+        }
+
+        ll m = (l + r) / 2;
+
+        if (l <= ql && qr <= m)
+        {
+            return query(2 * node, l, m, ql, qr);
+        }
+
+        else if (m + 1 <= ql && qr <= r)
+        {
+            return query(2 * node + 1, m + 1, r, ql, qr);
         }
 
         else
         {
-            ll mid = (l + r) / 2;
-
-            build_sum(arr, 2 * u, l, mid);
-            build_sum(arr, 2 * u + 1, mid + 1, r);
-
-            tree_sum[u] = tree_sum[2 * u] + tree_sum[2 * u + 1];
-        }
-    }
-
-    void build_prod(vector<ll> &arr, ll u, ll l, ll r)
-    {
-        if (l == r)
-        {
-            tree_prod[u] = arr[l];
-        }
-        else
-        {
-            ll mid = (l + r) / 2;
-
-            build_prod(arr, 2 * u, l, mid);
-            build_prod(arr, 2 * u + 1, mid + 1, r);
-
-            tree_prod[u] = tree_prod[2 * u] * tree_prod[2 * u + 1];
+            Info res_left = query(2 * node, l, m, ql, m);
+            Info res_right = query(2 * node + 1, m + 1, r, m + 1, qr);
+            return op(res_left, res_right);
         }
     }
 
 public:
-    void build_min(vector<ll> &arr)
+    SegmentTree(ll N)
     {
-        n = arr.size();
-        tree_min.resize(4 * n);
-
-        if (n != 0)
-        {
-            build_min(arr, 1, 0, n - 1);
-        }
+        this->n = N;
+        tree.resize(4 * n);
     }
 
-    void build_max(vector<ll> &arr)
+    SegmentTree(ll N, Info e)
     {
-        n = arr.size();
-        tree_max.resize(4 * n);
-
-        if (n != 0)
-        {
-            build_max(arr, 1, 0, n - 1);
-        }
+        this->n = N;
+        tree.resize(4 * n, e);
     }
 
-    void build_sum(vector<ll> &arr)
+    SegmentTree(vector<ll> &a)
     {
-        n = arr.size();
-        tree_sum.resize(4 * n);
-
-        if (n != 0)
-        {
-            build_sum(arr, 1, 0, n - 1);
-        }
+        this->n = (ll)a.size();
+        tree.resize(4 * n);
+        build(1, 0, n - 1, a);
     }
 
-    void build_prod(vector<ll> &arr)
+    Info query(ll l, ll r)
     {
-        n = arr.size();
-        tree_prod.resize(4 * n);
-
-        if (n != 0)
-        {
-            build_prod(arr, 1, 0, n - 1);
-        }
+        return query(1, 0, n - 1, l, r);
     }
 
-    ll query_min(ll ql, ll qr, ll l = 0, ll r = -1, ll u = 1)
+    void update(ll idx, ll val)
     {
-        if (r == -1)
-        {
-            r = n - 1;
-        }
-
-        if (l == ql && r == qr)
-        {
-            return tree_min[u];
-        }
-
-        ll mid = (l + r) / 2;
-
-        if (ql >= l && qr <= mid)
-        {
-            return query_min(ql, qr, l, mid, 2 * u);
-        }
-
-        else if (ql >= mid + 1 && qr <= r)
-        {
-            return query_min(ql, qr, mid + 1, r, 2 * u + 1);
-        }
-
-        else
-        {
-            return min(query_min(ql, mid, l, mid, 2 * u), query_min(mid + 1, qr, mid + 1, r, 2 * u + 1));
-        }
-    }
-
-    ll query_max(ll ql, ll qr, ll l = 0, ll r = -1, ll u = 1)
-    {
-        if (r == -1)
-        {
-            r = n - 1;
-        }
-
-        if (l == ql && r == qr)
-        {
-            return tree_max[u];
-        }
-
-        ll mid = (l + r) / 2;
-
-        if (ql >= l && qr <= mid)
-        {
-            return query_max(ql, qr, l, mid, 2 * u);
-        }
-
-        else if (ql >= mid + 1 && qr <= r)
-        {
-            return query_max(ql, qr, mid + 1, r, 2 * u + 1);
-        }
-
-        else
-        {
-            return max(query_max(ql, mid, l, mid, 2 * u), query_max(mid + 1, qr, mid + 1, r, 2 * u + 1));
-        }
-    }
-
-    ll query_sum(ll ql, ll qr, ll l = 0, ll r = -1, ll u = 1)
-    {
-        if (r == -1)
-        {
-            r = n - 1;
-        }
-
-        if (l == ql && r == qr)
-        {
-            return tree_sum[u];
-        }
-
-        ll mid = (l + r) / 2;
-
-        if (ql >= l && qr <= mid)
-        {
-            return query_sum(ql, qr, l, mid, 2 * u);
-        }
-
-        else if (ql >= mid + 1 && qr <= r)
-        {
-            return query_sum(ql, qr, mid + 1, r, 2 * u + 1);
-        }
-
-        else
-        {
-            return query_sum(ql, mid, l, mid, 2 * u) + query_sum(mid + 1, qr, mid + 1, r, 2 * u + 1);
-        }
-    }
-
-    ll query_prod(ll ql, ll qr, ll l = 0, ll r = -1, ll u = 1)
-    {
-        if (r == -1)
-        {
-            r = n - 1;
-        }
-
-        if (l == ql && r == qr)
-        {
-            return tree_prod[u];
-        }
-
-        ll mid = (l + r) / 2;
-
-        if (ql >= l && qr <= mid)
-        {
-            return query_prod(ql, qr, l, mid, 2 * u);
-        }
-
-        else if (ql >= mid + 1 && qr <= r)
-        {
-            return query_prod(ql, qr, mid + 1, r, 2 * u + 1);
-        }
-
-        else
-        {
-            return query_prod(ql, mid, l, mid, 2 * u) * query_prod(mid + 1, qr, mid + 1, r, 2 * u + 1);
-        }
-    }
-
-    void update(ll pos, ll new_value, ll l = 0, ll r = -1, ll u = 1)
-    {
-        if (r == -1)
-        {
-            r = n - 1;
-        }
-
-        if (l == r)
-        {
-            tree_min[u] = tree_max[u] = tree_sum[u] = tree_prod[u] = new_value;
-        }
-
-        else
-        {
-            ll mid = (l + r) / 2;
-
-            if (pos <= mid)
-            {
-                update(pos, new_value, l, mid, 2 * u);
-            }
-
-            else
-            {
-                update(pos, new_value, mid + 1, r, 2 * u + 1);
-            }
-
-            tree_min[u] = min(tree_min[2 * u], tree_min[2 * u + 1]);
-            tree_max[u] = max(tree_max[2 * u], tree_max[2 * u + 1]);
-            tree_sum[u] = tree_sum[2 * u] + tree_sum[2 * u + 1];
-            tree_prod[u] = tree_prod[2 * u] * tree_prod[2 * u + 1];
-        }
+        update(1, 0, n - 1, idx, val);
     }
 };
